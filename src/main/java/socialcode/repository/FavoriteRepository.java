e690d8ca3baacc6044bf127afa3cf50aaeb96155
Abdelrhman walid <abdelrhmanwalid1@gmail.com>
2015-05-14 20:41:30 +0200

    deleted old unused classes

--- src/main/java/socialcode/repository/FavoriteRepository.java
+++
@@ -1,9 +0,0 @@
-//package socialcode.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import socialcode.model.Favorite;
-//
-//@Repository("FavoriteRepository")
-//public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
-//}
